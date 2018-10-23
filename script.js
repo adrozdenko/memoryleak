7f71b75b4f1eda3a59799567749fee47ba09039c
orest.ivanychuk <orestysorestys@gmail.com>
2018-10-23 22:04:26 +0300

    Memory leak script app added

---
+++ script.js
@@ -0,0 +1,20 @@
+let x = [];
+let runing = false;
+const leakButton = document.getElementById('leak-button');
+const stopButton = document.getElementById('stop-button');
+
+function grow(){
+  x.push(new Array(1000000).join('x'));
+  if(running) {
+    setTimeout(grow, 1000)
+  }
+}
+
+leakButton.addEventListener('click', () => {
+  running = true;
+  grow();
+});
+
+leakButton.addEventListener('click', () => {
+  running = false;
+});
